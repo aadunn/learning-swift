982548f8d399e4f71b6f844320885a3ac7460bcb
Alex Dunn <alex@swarmio.media>
2023-10-24 13:01:20 -0400

    completed building lists and navigation, section 2

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Alex Dunn on 2023-10-24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}

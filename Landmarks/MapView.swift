af8c9afccab3f87a58a5eaf11ae397d653e2d8c5
Alex Dunn <alex@swarmio.media>
2023-10-24 12:13:19 -0400

    finished landmarks tutorial

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -10,8 +10,19 @@ import MapKit
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if #available(iOS 17.0, *) {
+            Map(initialPosition: .region(region))
+        } else {
+            Map(coordinateRegion: $region)
+        }
     }
+    
+    @State private var region: MKCoordinateRegion =
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    
 }
 
 struct MapView_Previews: PreviewProvider {

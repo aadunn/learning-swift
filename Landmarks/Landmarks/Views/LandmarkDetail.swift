d131aa42d0f69825690222cfb302701eaa4023f4
Alex Dunn <alex@swarmio.media>
2023-10-24 13:29:47 -0400

    completed building lists and navigation, section 6

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Alex Dunn on 2023-10-24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}

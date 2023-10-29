b9f74a4cd08e89396fae77b8308e4b7ece9cd1ae
Alex Dunn <alex@swarmio.media>
2023-10-28 21:42:00 -0400

    completed composing complex interfaces, added a tab view to complete the navigation hierarchy

---
+++ Landmarks/Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Alex Dunn on 2023-10-26.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, landmarks: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
---
+++ Landmarks/Landmarks/Views/CategoryRow.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Alex Dunn on 2023-10-27.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var landmarks: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(landmarks) {landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        landmarks: Array(landmarks.prefix(4))
+    )
+}
